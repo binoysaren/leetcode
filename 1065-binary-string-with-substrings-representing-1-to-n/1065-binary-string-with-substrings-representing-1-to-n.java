7aa1c5e1f0b9b18ba9694b23ad5c385cfd65e133
binoysaren <141212647+binoysaren@users.noreply.github.com>
2024-09-14 22:41:53 +0530

    Time: 0 ms (100%), Space: 41.4 MB (47.26%) - LeetHub

--- 1065-binary-string-with-substrings-representing-1-to-n/1065-binary-string-with-substrings-representing-1-to-n.java
+++ 1065-binary-string-with-substrings-representing-1-to-n/1065-binary-string-with-substrings-representing-1-to-n.java
@@ -1,20 +1,27 @@
 class Solution {
     public boolean queryString(String s, int n) {
-        int number = 0;
-        int len = s.length();
-        for (int i = 1; i <= n; i++) {
-            if (!s.contains(f(i))) {
-                return false;
-            }
+        // int number = 0;
+        // int len = s.length();
+        // for (int i = 1; i <= n; i++) {
+        // if (!s.contains(f(i))) {
+        // return false;
+        // }
+        // }
+        // return true;
+        // }
+        // public String f(int a) {
+        // String b = "";
+        // while (a > 0) {
+        // b = (a % 2) + b;
+        // a = a / 2;
+        // }
+        // return b;
+        for(int i=1;i<=n;i++){
+           String binoy=Integer.toBinaryString(i);
+           if(!s.contains(binoy)){
+            return false;
+           }
         }
         return true;
     }
-    public String f(int a) {
-    String b = "";
-    while (a > 0) {
-        b = (a % 2) + b; 
-        a = a / 2;
-    }
-    return b;
-}
 }
