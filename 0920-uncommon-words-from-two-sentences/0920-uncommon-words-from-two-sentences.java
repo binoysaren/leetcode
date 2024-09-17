b71e1fa1d407a3972fcf8a3dddc2298d050a0392
binoysaren <141212647+binoysaren@users.noreply.github.com>
2024-09-17 12:11:27 +0530

    Time: 5 ms (20.24%), Space: 42.3 MB (22.88%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
@@ -1,11 +1,11 @@
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
         HashMap<String,Integer> hm=new HashMap<>();
-        for(var s: (s1+" "+s2).split(" ")){
+        for(String s: (s1+" "+s2).split(" ")){
             hm.put(s,hm.getOrDefault(s,0)+1);
         }
         List<String> list=new ArrayList<>();
-        for(var s: hm.keySet()){
+        for(String s: hm.keySet()){
           if(hm.get(s)==1){
             list.add(s);
           }
