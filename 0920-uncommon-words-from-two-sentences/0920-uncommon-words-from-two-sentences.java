efad6d8b45de67f07a7bde4c152cbfed84e29810
binoysaren <141212647+binoysaren@users.noreply.github.com>
2024-09-17 12:10:28 +0530

    Time: 5 ms (20.24%), Space: 42.3 MB (13.76%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
@@ -1,35 +1,15 @@
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
-        String[] str1=s1.split(" ");
-        String[] str2=s2.split(" ");
-        List<String> p=new ArrayList<>();
-        List<String> q=new ArrayList<>();
-        for(int i=0;i<str1.length;i++){
-            p.add(str1[i]);
-        }
-        for(int i=0;i<str2.length;i++){
-            q.add(str2[i]);
-        }
-        HashMap<String, Integer> frequency1 = new HashMap<>();
-        for (String word : str1) {
-            frequency1.put(word, frequency1.getOrDefault(word, 0) + 1);
-        }
-        HashMap<String, Integer> frequency2= new HashMap<>();
-        for (String word : str2) {
-            frequency2.put(word, frequency2.getOrDefault(word, 0) + 1);
+        HashMap<String,Integer> hm=new HashMap<>();
+        for(var s: (s1+" "+s2).split(" ")){
+            hm.put(s,hm.getOrDefault(s,0)+1);
         }
         List<String> list=new ArrayList<>();
-        for(String s:str1){
-            if(!q.contains(s) && frequency1.get(s)==1){
-              list.add(s);
-            }
-        }
-        for(String s:str2){
-           if(!p.contains(s) && frequency2.get(s)==1){
+        for(var s: hm.keySet()){
+          if(hm.get(s)==1){
             list.add(s);
-           }
+          }
         }
-        String[] ans=list.toArray(new String[0]);
-        return ans;
+        return list.toArray(new String[0]);
     }
 }
