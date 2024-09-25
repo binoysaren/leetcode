c93b1c159773de4cc4e68f6865e445eacdb77c6d
binoysaren <141212647+binoysaren@users.noreply.github.com>
2024-09-25 15:25:24 +0530

    Time: 582 ms (11.61%), Space: 267 MB (7.1%) - LeetHub

---
+++ 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] sumPrefixScores(String[] words) {
+        Trie trie = new Trie();
+        int[] result = new int[words.length];
+        for (String word : words) {
+            trie.insert(word);
+        }
+        for (int i = 0; i < words.length; i++) {
+            result[i] = trie.f(words[i]);
+        }
+        return result; 
+    }
+}
+class TrieNode {
+    Map<Character, TrieNode> children = new HashMap<>();
+    int prefixCount = 0;
+}
+class Trie {
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            node.children.putIfAbsent(ch, new TrieNode());
+            node = node.children.get(ch);
+            node.prefixCount++;
+        }
+    }
+    public int f(String word) {
+        TrieNode node = root;
+        int score = 0;
+        for (char ch : word.toCharArray()) {
+            node = node.children.get(ch);
+            score += node.prefixCount;
+        }
+        return score;
+    }
+}
