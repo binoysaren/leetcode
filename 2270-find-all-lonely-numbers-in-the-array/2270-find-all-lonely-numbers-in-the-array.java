0b6706e2d0c1b31c11cdb013560904a371615b36
binoysaren <141212647+binoysaren@users.noreply.github.com>
2024-09-30 01:10:46 +0530

    Time: 90 ms (11.84%), Space: 63.7 MB (82.45%) - LeetHub

--- 2270-find-all-lonely-numbers-in-the-array/2270-find-all-lonely-numbers-in-the-array.java
+++ 2270-find-all-lonely-numbers-in-the-array/2270-find-all-lonely-numbers-in-the-array.java
@@ -1,14 +1,12 @@
 class Solution {
     public List<Integer> findLonely(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
         for (int i : nums) {
-            set.add(i);
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
         List<Integer> list = new ArrayList<>();
-        for (int i : nums) {
-            if (map.get(i) == 1 && !set.contains(i - 1) && !set.contains(i + 1)) {
+        for (int i : map.keySet()) {
+            if (map.get(i) == 1 && !map.containsKey(i - 1) && !map.containsKey(i + 1)) {
                 list.add(i);
             }
         }
