6893a9dee716f8fa0fe632e1f868f1151422014a
binoysaren <141212647+binoysaren@users.noreply.github.com>
2024-09-29 15:03:11 +0530

    Time: 61 ms (18.75%), Space: 60.6 MB (52.3%) - LeetHub

---
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.java
@@ -0,0 +1,35 @@
+class AllOne {
+    Map<String, Integer> map;
+    TreeSet<Pair<Integer, String>> set;
+    public AllOne() {
+        map = new HashMap<>();
+        set = new TreeSet<>(
+                (a, b) -> a.getKey().equals(b.getKey())
+                        ? a.getValue().compareTo(b.getValue())
+                        : a.getKey() - b.getKey());
+    }
+    public void inc(String key) {
+        int n = map.getOrDefault(key, 0);
+        map.put(key, n + 1);
+
+        set.remove(new Pair<>(n, key));
+        set.add(new Pair<>(n + 1, key));
+    }
+    public void dec(String key) {
+        int n = map.get(key);
+
+        set.remove(new Pair<>(n, key));
+        if (n == 1) {
+            map.remove(key);
+        } else {
+            map.put(key, n - 1);
+            set.add(new Pair<>(n - 1, key));
+        }
+    }
+    public String getMaxKey() {
+        return set.isEmpty() ? "" : set.last().getValue();
+    }
+    public String getMinKey() {
+        return set.isEmpty() ? "" : set.first().getValue();
+    }
+}
