2893f03249e31290b900dd70007212634bc47f2f
binoysaren <141212647+binoysaren@users.noreply.github.com>
2024-10-22 09:56:33 +0530

    Time: 50 ms (9.6%), Space: 63.6 MB (40.4%) - LeetHub

--- 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.java
+++ 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.java
@@ -1,32 +1,14 @@
 class Solution {
     public long kthLargestLevelSum(TreeNode root, int k) {
-        List<List<Integer>> list = new ArrayList<>();
-        levelorder(root, list);
         List<Long> temp = new ArrayList<>();
-        for (List<Integer> li : list) {
-            long sum = 0;
-            for (int i : li) {
-                sum += i;
-            }
-            temp.add(sum);
-        }
-        Collections.sort(temp);
-        System.out.println(temp);
-        if(k>temp.size()) return -1;
-        return temp.get(temp.size()-k);
-    }
-
-    public void levelorder(TreeNode root, List<List<Integer>> list) {
-        if (root == null)
-            return;
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
         while (!q.isEmpty()) {
-            List<Integer> l=new ArrayList<>();
-             int n = q.size();
+            long sum=0;
+            int n=q.size();
             for (int i = 0; i < n; i++) {
                 TreeNode tree = q.poll();
-                l.add(tree.val);
+                sum += tree.val;
                 if (tree.left != null) {
                     q.add(tree.left);
                 }
@@ -34,7 +16,12 @@ class Solution {
                     q.add(tree.right);
                 }
             }
-            list.add(l);
+            temp.add(sum);
         }
+        if (k > temp.size())
+            return -1;
+        Collections.sort(temp);
+        System.out.println(temp);
+        return temp.get(temp.size() - k);
     }
 }
