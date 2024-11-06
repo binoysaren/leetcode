eb5924aaaa18649366c6133dd3c387cfb77fda5a
binoysaren <141212647+binoysaren@users.noreply.github.com>
2024-11-06 19:45:02 +0530

    Time: 138 ms (6.65%), Space: 45.3 MB (53.01%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,37 +1,24 @@
-/**
- * One-Pass Constant-Space solution. Input board is not modified
- *
- * Going over all cells, we can count only those that are the "last" cell of the
- * battleship. Last cell will be defined as the most bottom-right cell. We can
- * check for last cells by only counting cells that do not have an 'X' to the
- * right and do not have an 'X' below them.
- *
- * Time Complexity: O(M * N)
- *
- * Space Complexity: O(1)
- *
- * M = Number of rows in the board. N = Number of columns in the board.
- */
 class Solution {
     public int countBattleships(char[][] board) {
-        if (board == null) {
-            throw new IllegalArgumentException("Input is null");
-        }
-        if (board.length == 0 || board[0].length == 0) {
-            return 0;
-        }
-
-        int rows = board.length;
-        int cols = board[0].length;
         int count = 0;
+        int rows = board.length;
+        int columns = board[0].length;
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (board[i][j] == 'X'
-						&& (j == cols - 1 || board[i][j + 1] == '.')
-                        && (i == rows - 1 || board[i + 1][j] == '.')) {
+        for (int i = 0; i<rows; i++){
+            int j = 0;
+            while(j<=columns){
+                while(j<columns && board[i][j]!='X'){
+                    j++;
+                }
+                boolean candidate = false;
+                if (j<columns && board[i][j] =='X'&& (i==0 || board[i-1][j]!='X')){
                     count++;
                 }
+                j++;
+                while(j<columns && board[i][j]=='X'){
+                    j++;
+                }
+
             }
         }
 
