2ce224746774a5056b0ed9159b22210e6029552b
binoysaren <141212647+binoysaren@users.noreply.github.com>
2024-11-12 16:53:07 +0530

    Time: 43 ms (92.75%), Space: 81.5 MB (37.28%) - LeetHub

---
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] maximumBeauty(int[][] items, int[] queries) {
+        int n = queries.length;
+        int[] ans = new int[n];
+        Arrays.sort(items, (a, b) -> a[0] - b[0]);
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < items.length; i++) {
+            maxi = Math.max(maxi, items[i][1]);
+            items[i][1] = maxi;
+        }
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = items.length - 1;
+            int maxFoundBeauty = 0;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (items[mid][0] <= queries[i]) {
+                    maxFoundBeauty = items[mid][1];
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            ans[i] = maxFoundBeauty;
+        }
+        return ans;
+    }
+}
