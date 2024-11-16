e398626ccedbf268b303bf81ddf8e9e743656412
binoysaren <141212647+binoysaren@users.noreply.github.com>
2024-11-16 11:19:22 +0530

    Time: 4 ms (19.08%), Space: 45.3 MB (81.91%) - LeetHub

---
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] resultsArray(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+        int index = 0;
+        for (int i = 0; i <= n - k; i++) {
+            int maxi = nums[i];
+            boolean flag = true;
+            for (int j = i; j < i + k - 1; j++) {
+                if (nums[j] + 1 != nums[j + 1]) {
+                    flag=false;
+                }
+                maxi=Math.max(maxi,nums[j+1]);
+            }
+            ans[index++]=!flag?-1:maxi;
+        }
+        return ans;
+    }
+}
