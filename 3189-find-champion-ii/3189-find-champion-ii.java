846e8b5a3ede58ffed47678368fbbe8438d50001
binoysaren <141212647+binoysaren@users.noreply.github.com>
2024-11-26 13:03:09 +0530

    Time: 1 ms (100%), Space: 45.6 MB (74.26%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.java
+++ 3189-find-champion-ii/3189-find-champion-ii.java
@@ -1,13 +1,11 @@
 class Solution {
     public int findChampion(int n, int[][] edges) {
-        int[] out=new int[n];
         int[] in=new int[n];
         for(int[] i:edges){
-            out[i[0]]++;
             in[i[1]]++;
         }
         int incoming=0;
-        int ans=0;
+        int ans=-1;
         for(int i=0;i<n;i++){
             if(in[i]==0){
                 incoming++;
