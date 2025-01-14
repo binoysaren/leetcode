07871e80cc99384e5f902d2ded46d2cdb2180742
binoysaren <141212647+binoysaren@users.noreply.github.com>
2025-01-14 12:26:40 +0530

    Time: 19 ms (12.76%), Space: 45.8 MB (16.93%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
@@ -1,20 +1,24 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
         int n=A.length;
-        int[] C=new int[n];
-        int i=0;
-        while(i<n){
-            int count=0;
-            for(int j=0;j<=i;j++){
-                for(int k=0;k<=i;k++){
-                    if(A[j]==B[k]){
-                        count++;
-                    }
-                }
-            }
-            C[i]=count;
-            i++;
+        int[] ans=new int[n];
+        Map<Integer,Integer> map1=new HashMap<>();
+        Map<Integer,Integer> map2=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map1.put(A[i],map1.getOrDefault(A[i],0)+1);
+            map2.put(B[i],map2.getOrDefault(B[i],0)+1);
+            // System.out.println(map1 + "  "+ map2);
+            ans[i]=f(map1,map2);
         }
-        return C;
+        return ans;
+    }
+    public int f(Map<Integer,Integer> map1,Map<Integer,Integer> map2){
+        int count=0;
+        for(int i:map1.keySet()){
+            if(map2.containsKey(i)){
+                count++;
+            }
+        }
+        return count;
     }
 }
