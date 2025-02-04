fe8167ca9bc23d949654a3ccf07e62de04e51257
binoysaren <141212647+binoysaren@users.noreply.github.com>
2025-02-04 18:16:14 +0530

    Time: 1 ms (100%), Space: 45 MB (42.91%) - LeetHub

--- 3337-count-substrings-starting-and-ending-with-given-character/3337-count-substrings-starting-and-ending-with-given-character.java
+++ 3337-count-substrings-starting-and-ending-with-given-character/3337-count-substrings-starting-and-ending-with-given-character.java
@@ -1,16 +1,14 @@
 class Solution {
     public long countSubstrings(String s, char c) {
-        Map<Character,Integer> map=new HashMap<>();
         int n=s.length();
+        long count=0;
         for(int i=0;i<n;i++){
             char ch=s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }  
-        if(!map.containsKey(c)){
-            return 0;
-        }
-        long ans=map.get(c);
-        return (ans*(ans-1)/2)+ans;
+            if(ch==c){
+                count++;
+            }
+        }   
+        return (count*(count-1)/2)+count;
     }
    
 }
