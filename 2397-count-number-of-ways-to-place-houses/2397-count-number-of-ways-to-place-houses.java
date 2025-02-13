42489fd7d2b6e7e1abaa95581a553584d38b530e
binoysaren <141212647+binoysaren@users.noreply.github.com>
2025-02-13 16:14:16 +0530

    Time: 4 ms (95.78%), Space: 40.3 MB (96.39%) - LeetHub

--- 2397-count-number-of-ways-to-place-houses/2397-count-number-of-ways-to-place-houses.java
+++ 2397-count-number-of-ways-to-place-houses/2397-count-number-of-ways-to-place-houses.java
@@ -1,15 +1,16 @@
-import java.math.BigInteger;
 class Solution {
     public int countHousePlacements(int n) {
-        BigInteger mod = BigInteger.valueOf(1000000007); 
-        if (n == 1) return 4;
-        if (n == 2) return 9;
-        BigInteger[] dp = new BigInteger[n + 1];
-        dp[1] = BigInteger.valueOf(2);
-        dp[2] = BigInteger.valueOf(3);
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1].add(dp[i - 2]);
-        }
-        return dp[n].multiply(dp[n]).mod(mod).intValue();
+       if(n==1) return 4;
+       if(n==2) return 9;
+       long mod=1000000007;
+       long a=2;
+       long b=3;
+       long c=0;
+       for(int i=3;i<=n;i++){
+          c=(a+b)%mod;
+          a=b;
+          b=c;
+       } 
+       return (int)((1L*c*c)%mod);
     }
 }
