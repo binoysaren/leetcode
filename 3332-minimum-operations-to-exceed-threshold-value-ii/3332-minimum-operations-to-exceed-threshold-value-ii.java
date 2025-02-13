d0407f527d24a38834cbe96d5af8ed5a782bd7a7
binoysaren <141212647+binoysaren@users.noreply.github.com>
2025-02-13 11:42:40 +0530

    Time: 158 ms (11.11%), Space: 70.5 MB (69.96%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
@@ -5,14 +5,14 @@ class Solution {
             pq.add((long)i);
         }
         int count=0;
-        while(pq.size()>1 && pq.peek()<k){
+        while(pq.peek()<k){
            long x=pq.poll();
            long y=pq.poll();
-           count++;
            long z=Math.min(x,y)*2+Math.max(x,y);
            pq.offer(z);
+            count++;
         }
-         System.out.println(pq);
+        //  System.out.println(pq);
         return count;
     }
 }
