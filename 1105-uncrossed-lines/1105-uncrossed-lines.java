24a5760dcc67a894f65ffa63a35adb7ee6ffd512
binoysaren <141212647+binoysaren@users.noreply.github.com>
2025-02-17 20:06:39 +0530

    Time: 7 ms (27.07%), Space: 43.8 MB (89.86%) - LeetHub

--- 1105-uncrossed-lines/1105-uncrossed-lines.java
+++ 1105-uncrossed-lines/1105-uncrossed-lines.java
@@ -2,7 +2,6 @@ class Solution {
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
         int n1=nums1.length;
         int n2=nums2.length;
-        // Set<Integer> set=new HashSet<>();
         int[][] dp=new int[n1+1][n2+1];
         for(int i=0;i<dp.length;i++){
             for(int j=0;j<dp[i].length;j++){
