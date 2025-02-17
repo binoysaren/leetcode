ee9b10388a5dd66a2bbcf44a4a09207dff1c215e
binoysaren <141212647+binoysaren@users.noreply.github.com>
2025-02-17 22:55:54 +0530

    Time: 21 ms (64.39%), Space: 45.8 MB (11.04%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -23,12 +23,12 @@ class Solution {
                 }
             }
         }
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[i].length;j++){
-                System.out.print(dp[i][j]+ " ");
-            }
-            System.out.println();
-        }
+        // for(int i=0;i<dp.length;i++){
+        //     for(int j=0;j<dp[i].length;j++){
+        //         System.out.print(dp[i][j]+ " ");
+        //     }
+        //     System.out.println();
+        // }
         return (int)dp[n2][n1];
     }
 }
