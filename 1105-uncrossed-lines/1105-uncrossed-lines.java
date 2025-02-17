1387432db872c37b72b56e07bd76b1f8fc48d60a
binoysaren <141212647+binoysaren@users.noreply.github.com>
2025-02-17 20:06:23 +0530

    Time: 6 ms (67.74%), Space: 43.8 MB (86.28%) - LeetHub

---
+++ 1105-uncrossed-lines/1105-uncrossed-lines.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int n1=nums1.length;
+        int n2=nums2.length;
+        // Set<Integer> set=new HashSet<>();
+        int[][] dp=new int[n1+1][n2+1];
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[i].length;j++){
+                if(i==0){
+                    dp[i][j]=0;
+                }
+                else if(j==0){
+                    dp[i][j]=0;
+                }
+                else if(nums1[i-1]==nums2[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+            
+        }     
+        // for(int i=0;i<dp.length;i++){
+        //     for(int j=0;j<dp[i].length;j++){
+        //         System.out.print(dp[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // } 
+        return dp[n1][n2];
+    }
+} 
+//   0 1 9 2 5 1
+// 0 0 0 0 0 0 0
+// 1 0 1 1 1 1 1
+// 3 0 1 1 1 1 1
+// 7 0 1 1 1 1 1
+// 1 0 1 
+// 7 0
+// 5 0
