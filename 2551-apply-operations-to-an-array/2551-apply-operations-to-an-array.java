b6934d2027324ceb561293e744cce439f212e1e6
binoysaren <141212647+binoysaren@users.noreply.github.com>
2025-03-01 11:22:57 +0530

    Time: 1 ms (96.37%), Space: 44.5 MB (8.68%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,20 +1,18 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
         int n = nums.length;
-        Stack<Integer> st = new Stack<>();
         for (int i = 0; i < n; i++) {
             if (i + 1 < n && nums[i] == nums[i + 1]) {
                 nums[i] *= 2; 
                 nums[i + 1] = 0; 
             }
-            if (nums[i] != 0) {
-                st.push(nums[i]); 
-            }
         }
         int[] ans = new int[n];
-        int idx = 0;
-        while (!st.isEmpty()) {
-            ans[idx++] = st.remove(0); 
+        int index=0;
+        for(int i:nums){
+            if(i!=0){
+                ans[index++]=i;
+            }
         }
         return ans;
     }
