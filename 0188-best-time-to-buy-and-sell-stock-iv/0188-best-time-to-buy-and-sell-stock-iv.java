f398d21b0dff5e508a671afc5c1f8790ee574412
binoysaren <141212647+binoysaren@users.noreply.github.com>
2025-03-19 20:17:51 +0530

    Time: 4 ms (49.72%), Space: 42.3 MB (70.98%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -3,12 +3,13 @@ class Solution {
         int n=prices.length;
         int[][] dp=new int[k+1][n];
         for(int i=1;i<k+1;i++){
+             int maxi=Integer.MIN_VALUE;
             for(int j=1;j<n;j++){
-                int maxi=Integer.MIN_VALUE;
-                for(int index=0;index<j;index++){
-                    maxi=Math.max(maxi,dp[i-1][index]+prices[j]-prices[index]);
-                }
-                dp[i][j]=Math.max(dp[i][j-1],maxi);
+                // for(int index=0;index<j;index++){
+                //     maxi=Math.max(maxi,dp[i-1][index]+prices[j]-prices[index]);
+                // }
+                maxi=Math.max(maxi,dp[i-1][j-1]-prices[j-1]);
+                dp[i][j]=Math.max(dp[i][j-1],maxi+prices[j]);
             }
         }
         return dp[k][n-1];
